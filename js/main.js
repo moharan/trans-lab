efab441490d016411d61d7a7309381805d5240d3
RangiBecerra <rangibecerra@gmail.com>
2017-07-13 19:32:00 -0400

    css pagina y botones trabajando por consola

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
-/* Holiii acá va tu código también */
-//alert("hola");
+/* Código */
+$( document ).ready(function() {
+	//alert("hola");
+	function validateForm() {
+		console.log("working");
+		// llamada a la siguiente pagina
+		window.location.href = "pagina.html";
+	};
+	// llamada al boton en index.html
+	$('#iniciar').on('click', function (event) {
+    validateForm();
+	});
+	// llamada botones en pagina.html
+	$('#saldo').on('click', function (event) {
+    console.log("saldo");
+	});
+	$('#tarifa').on('click', function (event) {
+    console.log("tarifa");
+	});
+	$('#perfil').on('click', function (event) {
+    console.log("perfil");
+	});
+	$('#preguntas').on('click', function (event) {
+    console.log("preguntas");
+	});
+	/* Fin ready */
+});
